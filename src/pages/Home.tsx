9a3f2f150d6f4e88c8d82fbc096296d862492d91
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2023-11-02 17:38:45 +0100

    feat: routing - handle routing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from '@/layouts/Default'
+
+export default function HomePage() {
+  return (
+    <DefaultLayout>
+      <h1>home page</h1>
+    </DefaultLayout>
+  )
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import HomePage from '@/pages/Home'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  }
+])
+
+export default routes
