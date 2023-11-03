96788d6e9ed7193c9040970d271dd19491e44812
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2023-11-03 12:46:19 +0100

    feat: planets-page - handle planets page functionality

--- src/services/planets.ts
+++ src/services/planets.ts
@@ -1,6 +1,9 @@
 import apiFetch from '@/plugins/apiFetch'
 
-import { TPlanet } from '@/types/planets'
+import { TPlanet, TPlanetsRes } from '@/types/planets'
+
+export const fetchPlanets = (page: number): Promise<TPlanetsRes> =>
+  apiFetch(`/planets?page=${page}`)
 
 export const fetchPlanet = (id: string): Promise<TPlanet> =>
   apiFetch(`/planets/${id}`)
