619d8590202838cfa91d691f994c4f0833172ba2
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2023-11-03 12:56:03 +0100

    feat: planet-page - handle planet page functionality

--- src/pages/Planet/index.tsx
+++ src/pages/Planet/index.tsx
@@ -1,16 +1,40 @@
+import { Link, useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+
 import DefaultLayout from '@/layouts/Default'
 
+import { fetchPlanet } from '@/services/planets'
+import { getIdFromUrl } from '@/utils/helpers'
+
 export default function PlanetPage() {
+  const { id } = useParams()
+
+  const { data: planet } = useQuery({
+    queryKey: ['planet', id],
+    queryFn: () => fetchPlanet(id as string)
+  })
+
   return (
     <DefaultLayout>
       <section className="planet">
         <article>
           <header>
             <img src="https://placehold.co/150x150" alt="" />
-            <h1>planet</h1>
+            <h1>{planet?.name}</h1>
           </header>
-          <p>population count: </p>
-          <p>characters connected: </p>
+          <p>population count: {planet?.population}</p>
+          <p>
+            <span>characters connected:</span>
+            <div>
+              {planet?.residents?.map(resident => (
+                <div>
+                  <Link to={`/characters/${getIdFromUrl(resident)}`}>
+                    {resident}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </p>
         </article>
       </section>
     </DefaultLayout>
