fe81f6add4e88909edca9bdb6625c96f08fa6771
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2023-11-04 20:48:40 +0100

    fix: route-data - clear data between routes

--- src/pages/Vehicle/index.tsx
+++ src/pages/Vehicle/index.tsx
@@ -10,7 +10,7 @@ export default function VehiclePage() {
   const { id } = useParams()
 
   const { data: vehicle } = useMyQuery({
-    queryKey: ['planet', id],
+    queryKey: ['vehicle', id],
     queryFn: () => fetchVehicle(id as string)
   })
 
