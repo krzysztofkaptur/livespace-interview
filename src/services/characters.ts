0c90aa69cf191249bc8f4535e434c63c6063372e
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2023-11-05 12:52:06 +0100

    feat: abort-controller - abort requests

--- src/services/characters.ts
+++ src/services/characters.ts
@@ -2,8 +2,24 @@ import apiFetch from '@/plugins/apiFetch'
 
 import { TCharactersRes, TCharacter } from '@/types/characters'
 
-export const fetchCharacters = (page: number): Promise<TCharactersRes> =>
-  apiFetch(`/people?page=${page}`)
+export const fetchCharacters = ({
+  page,
+  signal
+}: {
+  page: number
+  signal: AbortSignal
+}): Promise<TCharactersRes> =>
+  apiFetch(`/people?page=${page}`, {
+    signal
+  })
 
-export const fetchCharacter = (id: string): Promise<TCharacter> =>
-  apiFetch(`/people/${id}`)
+export const fetchCharacter = ({
+  id,
+  signal
+}: {
+  id: string
+  signal: AbortSignal
+}): Promise<TCharacter> =>
+  apiFetch(`/people/${id}`, {
+    signal
+  })
--- src/services/vehicles.ts
+++ src/services/vehicles.ts
@@ -2,8 +2,24 @@ import apiFetch from '@/plugins/apiFetch'
 
 import { TVehicle, TVehiclesRes } from '@/types/vehicles'
 
-export const fetchVehicles = (page: number): Promise<TVehiclesRes> =>
-  apiFetch(`/vehicles?page=${page}`)
+export const fetchVehicles = ({
+  page,
+  signal
+}: {
+  page: number
+  signal: AbortSignal
+}): Promise<TVehiclesRes> =>
+  apiFetch(`/vehicles?page=${page}`, {
+    signal
+  })
 
-export const fetchVehicle = (id: string): Promise<TVehicle> =>
-  apiFetch(`/vehicles/${id}`)
+export const fetchVehicle = ({
+  id,
+  signal
+}: {
+  id: string
+  signal: AbortSignal
+}): Promise<TVehicle> =>
+  apiFetch(`/vehicles/${id}`, {
+    signal
+  })
